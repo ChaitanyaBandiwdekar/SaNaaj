5d1eb4c85eb4860e523cfaf8161f398992146c92
Pooja Kaulgud <pooja.kaulgud@somaiya.edu>
2022-04-28 20:04:33 +0530

    bhejnewaladata

--- contracts/Sanaaj.sol
+++ contracts/Sanaaj.sol
@@ -6,30 +6,30 @@ pragma experimental ABIEncoderV2;
 // Rice - 4kg, sugar - 0.5kg, wheat - 10kg
 
 library AssetLibrary {
-    struct Consumer {
-        string ration_card;
-        uint256 ration_card_type;
-        string first_name;
-        string last_name;
-        string phone;
-        string location;
-        uint256 vendor_id;
-        string password;
-        address wallet_addr;
-        uint256 num_of_adults;
-        uint256 num_of_children;
-    }
+    // struct Consumer {
+    //     string ration_card;
+    //     uint256 ration_card_type;
+    //     string first_name;
+    //     string last_name;
+    //     string phone;
+    //     string location;
+    //     uint256 vendor_id;
+    //     string password;
+    //     address wallet_addr;
+    //     uint256 num_of_adults;
+    //     uint256 num_of_children;
+    // }
 
-    struct Vendor {
-        uint256 vendor_id;
-        string first_name;
-        string last_name;
-        string phone;
-        string location;
-        string password;
-        bool isBlacklisted;
-        address wallet_addr;
-    }
+    // struct Vendor {
+    //     uint256 vendor_id;
+    //     string first_name;
+    //     string last_name;
+    //     string phone;
+    //     string location;
+    //     string password;
+    //     bool isBlacklisted;
+    //     address wallet_addr;
+    // }
 
     struct Transaction {
         string consumer_ration_id;
@@ -38,115 +38,109 @@ library AssetLibrary {
         string timestamp;
     }
 
-    struct Complaint {
-        string ration_id;
-        uint256 vendor_id;
-        string complaint_content;
-        string time;
-    }
+
 }
 
 contract Sanaaj {
     uint256 transactionCount;
-    mapping(string => AssetLibrary.Consumer) public consumerList;
-    mapping(uint256 => AssetLibrary.Vendor) public vendorList;
+    // mapping(string => AssetLibrary.Consumer) public consumerList;
+    // mapping(uint256 => AssetLibrary.Vendor) public vendorList;
     mapping(string => mapping(string => uint256)) public allowanceList;
     mapping(uint256 => mapping(string => uint256)) public stockList;
     mapping(string => AssetLibrary.Transaction[]) public transactionList;
-    mapping(string => AssetLibrary.Transaction[]) public pendingTransactionList;
-    AssetLibrary.Complaint[] public complaintList;
     string[] rationCardList;
+    mapping(uint256 => string[]) public newTransactionList;
     uint256[] vendorIDList;
     address adminAddress;
     string adminPassword;
 
     constructor() public {
-        consumerList["MH1234567890"] = AssetLibrary.Consumer(
-            "MH1234567890",
-            1,
-            "Ram",
-            "Ghorpade",
-            "1234567890",
-            "Mumbai",
-            1,
-            "0x134563d4e440f0e418b0f382f23a2cf301af6d7f648ccfae9895018345d779a3", //"Hello123"
-            0xFc43ee9441590F044136fC8B225483188a11de92,
-            2,
-            2
-        );
-        consumerList["MH1234509666"] = AssetLibrary.Consumer(
-            "MH1234509666",
-            2,
-            "Shyam",
-            "Ghorpade",
-            "1234509876",
-            "Mumbai",
-            1,
-            "0x75ca1baec3f3382989affc4cadb0cc93f47e946b247e3a655c6b36d43e64dd2a", //"Hello1234"
-            0xB793c1d9c9D5050859864DbD321Fc873e7942e54,
-            2,
-            3
-        );
-        consumerList["MH1234509876"] = AssetLibrary.Consumer(
-            "MH1234509876",
-            2,
-            "Ghanashyam",
-            "Deshpande",
-            "1234509876",
-            "Mumbai",
-            1,
-            "0xbaa0487be7b20f0a7c20804a38771626ec1b04476a9ac7d7b55ba1b80bc2ad2f", //"Hello1235"
-            0xb7C6562BF283534A775d5198195bfD8d47D22647,
-            3,
-            2
-        );
-        consumerList["MH9876541230"] = AssetLibrary.Consumer(
-            "MH9876541230",
-            1,
-            "Dagdu",
-            "Parab",
-            "9876541230",
-            "Mumbai",
-            2,
-            "0xa9df37c2d65ced953698bab7a72d9a31b4e7e25e2de561d1d127a8b06b6d70f3", //"Hello1236"
-            0xb6444A785Bc6d27d3fAcb648dc0714C39F569B24,
-            4,
-            2
-        );
-        consumerList["MH9876504321"] = AssetLibrary.Consumer(
-            "MH9876504321",
-            3,
-            "ABC",
-            "XYZ",
-            "9876504321",
-            "Mumbai",
-            2,
-            "facd7b3ff9742230f7202864686d424250d70957bd99d8475236f76d1a9e788b", //"Hello1237"
-            0x97b814cdcD7F542f413E2CFef8AF97c6633ffF3D,
-            2,
-            6
-        );
+        // consumerList["MH1234567890"] = AssetLibrary.Consumer(
+        //     "MH1234567890",
+        //     1,
+        //     "Ram",
+        //     "Ghorpade",
+        //     "1234567890",
+        //     "Mumbai",
+        //     1,
+        //     "0x134563d4e440f0e418b0f382f23a2cf301af6d7f648ccfae9895018345d779a3", //"Hello123"
+        //     0xFc43ee9441590F044136fC8B225483188a11de92,
+        //     2,
+        //     2
+        // );
+        // consumerList["MH1234509666"] = AssetLibrary.Consumer(
+        //     "MH1234509666",
+        //     2,
+        //     "Shyam",
+        //     "Ghorpade",
+        //     "1234509876",
+        //     "Mumbai",
+        //     1,
+        //     "0x75ca1baec3f3382989affc4cadb0cc93f47e946b247e3a655c6b36d43e64dd2a", //"Hello1234"
+        //     0xB793c1d9c9D5050859864DbD321Fc873e7942e54,
+        //     2,
+        //     3
+        // );
+        // consumerList["MH1234509876"] = AssetLibrary.Consumer(
+        //     "MH1234509876",
+        //     2,
+        //     "Ghanashyam",
+        //     "Deshpande",
+        //     "1234509876",
+        //     "Mumbai",
+        //     1,
+        //     "0xbaa0487be7b20f0a7c20804a38771626ec1b04476a9ac7d7b55ba1b80bc2ad2f", //"Hello1235"
+        //     0xb7C6562BF283534A775d5198195bfD8d47D22647,
+        //     3,
+        //     2
+        // );
+        // consumerList["MH9876541230"] = AssetLibrary.Consumer(
+        //     "MH9876541230",
+        //     1,
+        //     "Dagdu",
+        //     "Parab",
+        //     "9876541230",
+        //     "Mumbai",
+        //     2,
+        //     "0xa9df37c2d65ced953698bab7a72d9a31b4e7e25e2de561d1d127a8b06b6d70f3", //"Hello1236"
+        //     0xb6444A785Bc6d27d3fAcb648dc0714C39F569B24,
+        //     4,
+        //     2
+        // );
+        // consumerList["MH9876504321"] = AssetLibrary.Consumer(
+        //     "MH9876504321",
+        //     3,
+        //     "ABC",
+        //     "XYZ",
+        //     "9876504321",
+        //     "Mumbai",
+        //     2,
+        //     "facd7b3ff9742230f7202864686d424250d70957bd99d8475236f76d1a9e788b", //"Hello1237"
+        //     0x97b814cdcD7F542f413E2CFef8AF97c6633ffF3D,
+        //     2,
+        //     6
+        // );
 
-        vendorList[1] = AssetLibrary.Vendor(
-            1,
-            "Siddhesh",
-            "Bagwe",
-            "1029384756",
-            "Mumbai",
-            "0x9f66dc74cb77e162097a36ddc845799b69f7d721e4146b7f69c873ed9c96697e", //"hello987"
-            false,
-            0x675A7359e0d96b9A25DCA5d08e1f1Ee85B9cD24f
-        );
-        vendorList[2] = AssetLibrary.Vendor(
-            2,
-            "Pooja",
-            "Kaulgud",
-            "9087564132",
-            "Mumbai",
-            "15af607aded488ed5fd6d94c14cc4412bd58245f064ce8da28b486857eecf607", //"hello9876"
-            false,
-            0x295b6AE0Efe05de0011E9316E27B560cCdCD3DDb
-        );
+        // vendorList[1] = AssetLibrary.Vendor(
+        //     1,
+        //     "Siddhesh",
+        //     "Bagwe",
+        //     "1029384756",
+        //     "Mumbai",
+        //     "0x9f66dc74cb77e162097a36ddc845799b69f7d721e4146b7f69c873ed9c96697e", //"hello987"
+        //     false,
+        //     0x675A7359e0d96b9A25DCA5d08e1f1Ee85B9cD24f
+        // );
+        // vendorList[2] = AssetLibrary.Vendor(
+        //     2,
+        //     "Pooja",
+        //     "Kaulgud",
+        //     "9087564132",
+        //     "Mumbai",
+        //     "15af607aded488ed5fd6d94c14cc4412bd58245f064ce8da28b486857eecf607", //"hello9876"
+        //     false,
+        //     0x295b6AE0Efe05de0011E9316E27B560cCdCD3DDb
+        // );
 
         adminPassword = "0xac9689e2272427085e35b9d3e3e8bed88cb3434828b43b86fc0596cad4c6e270"; //"admin1234";
         rationCardList = [
@@ -164,41 +158,7 @@ contract Sanaaj {
         return adminPassword;
     }
 
-    function getConsumer(string memory _ration)
-        public
-        view
-        returns (AssetLibrary.Consumer memory)
-    {
-        return consumerList[_ration];
-    }
-
-    function getVendor(uint256 _id)
-        public
-        view
-        returns (AssetLibrary.Vendor memory)
-    {
-        return vendorList[_id];
-    }
-
-    function getAllConsumers() public view returns (string[] memory) {
-        // if (byVendor) {
-        //     string[] memory consumers;
-        //     uint256 count = 0;
-        //     for (uint256 i = 0; i < rationCardList.length; i++) {
-        //         if (consumerList[rationCardList[i]].vendor_id == _id) {
-        //             consumers[count] = rationCardList[i];
-        //             count++;
-        //         }
-        //     }
-        //     return consumers;
-        // }
-
-        return rationCardList;
-    }
-
-    function getAllVendors() public view returns (uint256[] memory) {
-        return vendorIDList;
-    }
+    // 
 
     function getAllowance(string memory _ration)
         public
@@ -264,6 +224,12 @@ contract Sanaaj {
         );
     }
 
+    function pushTransactions(string[] memory transactionList123, uint256 vendor_id) public{
+        for(uint i=0; i<transactionList123.length;i++){
+            newTransactionList[vendor_id].push(transactionList123[i]);
+        }
+    }
+
     
 
     function getTransactions(string memory _ration)
@@ -274,137 +240,50 @@ contract Sanaaj {
         return transactionList[_ration];
     }
 
-    function refillAllowance(string memory _ration) public {
+    // function refillAllowance(string memory _ration) public {
         
-        if (consumerList[_ration].ration_card_type == 1) {
-            allowanceList[_ration]["Rice"] = (consumerList[_ration].num_of_adults+consumerList[_ration].num_of_children)*5;
-            allowanceList[_ration]["Wheat"] = (consumerList[_ration].num_of_adults+consumerList[_ration].num_of_children)*5;
-            allowanceList[_ration]["Sugar"] = (consumerList[_ration].num_of_adults+consumerList[_ration].num_of_children)*1;
-            allowanceList[_ration]["Kerosene"] = (consumerList[_ration].num_of_adults+consumerList[_ration].num_of_children)*5;
-        } else if (consumerList[_ration].ration_card_type == 2) {
-            allowanceList[_ration]["Rice"] = (consumerList[_ration].num_of_adults+consumerList[_ration].num_of_children)*6;
-            allowanceList[_ration]["Wheat"] = (consumerList[_ration].num_of_adults+consumerList[_ration].num_of_children)*6;
-            allowanceList[_ration]["Sugar"] = (consumerList[_ration].num_of_adults+consumerList[_ration].num_of_children)*1;
-            allowanceList[_ration]["Kerosene"] = (consumerList[_ration].num_of_adults+consumerList[_ration].num_of_children)*6;
-        } else {
-            allowanceList[_ration]["Rice"] = (consumerList[_ration].num_of_adults+consumerList[_ration].num_of_children)*7;
-            allowanceList[_ration]["Wheat"] = (consumerList[_ration].num_of_adults+consumerList[_ration].num_of_children)*7;
-            allowanceList[_ration]["Sugar"] = (consumerList[_ration].num_of_adults+consumerList[_ration].num_of_children)*2;
-            allowanceList[_ration]["Kerosene"] = (consumerList[_ration].num_of_adults+consumerList[_ration].num_of_children)*7;
-        }
+    //     if (consumerList[_ration].ration_card_type == 1) {
+    //         allowanceList[_ration]["Rice"] = (consumerList[_ration].num_of_adults+consumerList[_ration].num_of_children)*5;
+    //         allowanceList[_ration]["Wheat"] = (consumerList[_ration].num_of_adults+consumerList[_ration].num_of_children)*5;
+    //         allowanceList[_ration]["Sugar"] = (consumerList[_ration].num_of_adults+consumerList[_ration].num_of_children)*1;
+    //         allowanceList[_ration]["Kerosene"] = (consumerList[_ration].num_of_adults+consumerList[_ration].num_of_children)*5;
+    //     } else if (consumerList[_ration].ration_card_type == 2) {
+    //         allowanceList[_ration]["Rice"] = (consumerList[_ration].num_of_adults+consumerList[_ration].num_of_children)*6;
+    //         allowanceList[_ration]["Wheat"] = (consumerList[_ration].num_of_adults+consumerList[_ration].num_of_children)*6;
+    //         allowanceList[_ration]["Sugar"] = (consumerList[_ration].num_of_adults+consumerList[_ration].num_of_children)*1;
+    //         allowanceList[_ration]["Kerosene"] = (consumerList[_ration].num_of_adults+consumerList[_ration].num_of_children)*6;
+    //     } else {
+    //         allowanceList[_ration]["Rice"] = (consumerList[_ration].num_of_adults+consumerList[_ration].num_of_children)*7;
+    //         allowanceList[_ration]["Wheat"] = (consumerList[_ration].num_of_adults+consumerList[_ration].num_of_children)*7;
+    //         allowanceList[_ration]["Sugar"] = (consumerList[_ration].num_of_adults+consumerList[_ration].num_of_children)*2;
+    //         allowanceList[_ration]["Kerosene"] = (consumerList[_ration].num_of_adults+consumerList[_ration].num_of_children)*7;
+    //     }
         
-    }
+    // }
 
-    function refillStock(uint256 _vendorID) public {
-        stockList[_vendorID]["Rice"] = 500;
-        stockList[_vendorID]["Wheat"] = 1000;
-        stockList[_vendorID]["Sugar"] = 500;
-        stockList[_vendorID]["Kerosene"] = 500;
-    }
+    // function refillStock(uint256 _vendorID) public {
+    //     stockList[_vendorID]["Rice"] = 500;
+    //     stockList[_vendorID]["Wheat"] = 1000;
+    //     stockList[_vendorID]["Sugar"] = 500;
+    //     stockList[_vendorID]["Kerosene"] = 500;
+    // }
 
-    function blacklist(uint256 _id) public {
-        vendorList[_id].isBlacklisted = !vendorList[_id].isBlacklisted;
-    }
+    // 
 
-    function updateCardType(string memory _ration, uint256 card_type) public {
-        consumerList[_ration].ration_card_type = card_type;
-    }
+    // function resetConsumerPassword(
+    //     string memory ration_id,
+    //     string memory new_password
+    // ) public {
+    //     string memory password1 = toHex(sha256(abi.encodePacked(new_password)));
+    //     consumerList[ration_id].password = password1;
+    // }
 
-    function addConsumer(
-        string memory ration_card,
-        uint256 ration_card_type,
-        string memory first_name,
-        string memory last_name,
-        string memory phone,
-        string memory location,
-        uint256 vendor_id,
-        string memory password,
-        address wallet_addr,
-        uint256 num_adults,
-        uint256 num_children
-    ) public {
-        // require(sha256(abi.encodePacked()) == sha256(abi.encodePacked()));
-        AssetLibrary.Consumer memory consumer = AssetLibrary.Consumer(
-            ration_card,
-            ration_card_type,
-            first_name,
-            last_name,
-            phone,
-            location,
-            vendor_id,
-            toHex(sha256(abi.encodePacked(password))),
-            wallet_addr,
-            num_adults,
-            num_children
-        );
-
-        consumerList[ration_card] = consumer;
-        rationCardList.push(ration_card);
-    }
-
-    function addVendor(
-        uint256 vendor_id,
-        string memory first_name,
-        string memory last_name,
-        string memory phone,
-        string memory location,
-        string memory password,
-        bool isBlacklisted,
-        address wallet_addr
-    ) public {
-        AssetLibrary.Vendor memory vendor = AssetLibrary.Vendor(
-            vendor_id,
-            first_name,
-            last_name,
-            phone,
-            location,
-            toHex(sha256(abi.encodePacked(password))),
-            isBlacklisted,
-            wallet_addr
-        );
-
-        vendorList[vendor_id] = vendor;
-        vendorIDList.push(vendor_id);
-    }
-
-    function addComplaint(
-        string memory ration_id,
-        uint256 vendor_id,
-        string memory complaint_content,
-        string memory time
-    ) public {
-        AssetLibrary.Complaint memory complaint = AssetLibrary.Complaint(
-            ration_id,
-            vendor_id,
-            complaint_content,
-            time
-        );
-
-        complaintList.push(complaint);
-    }
-
-    function getAllComplaints()
-        public
-        view
-        returns (AssetLibrary.Complaint[] memory)
-    {
-        return complaintList;
-    }
-
-    function resetConsumerPassword(
-        string memory ration_id,
-        string memory new_password
-    ) public {
-        string memory password1 = toHex(sha256(abi.encodePacked(new_password)));
-        consumerList[ration_id].password = password1;
-    }
-
-    function resetVendorPassword(uint256 vendor_id, string memory new_password)
-        public
-    {
-        string memory password1 = toHex(sha256(abi.encodePacked(new_password)));
-        vendorList[vendor_id].password = password1;
-    }
+    // function resetVendorPassword(uint256 vendor_id, string memory new_password)
+    //     public
+    // {
+    //     string memory password1 = toHex(sha256(abi.encodePacked(new_password)));
+    //     vendorList[vendor_id].password = password1;
+    // }
 
     function getAdminAddress()
         public
@@ -414,33 +293,7 @@ contract Sanaaj {
         return adminAddress;
     }
 
-    function checkConsumerCredentials(
-        string memory ration_id,
-        string memory password
-    ) public view returns (bool) {
-        AssetLibrary.Consumer memory consumer = consumerList[ration_id];
-
-        string memory hashed_password = toHex(
-            sha256(abi.encodePacked(password))
-        );
-        return (keccak256(abi.encodePacked((hashed_password))) ==
-            keccak256(abi.encodePacked((consumer.password))));
-    }
-
-    function checkVendorCredentials(uint256 vendor_id, string memory password)
-        public
-        view
-        returns (bool)
-    {
-        AssetLibrary.Vendor memory vendor = vendorList[vendor_id];
-
-        string memory hashed_password = toHex(
-            sha256(abi.encodePacked(password))
-        );
-        return (keccak256(abi.encodePacked((hashed_password))) ==
-            keccak256(abi.encodePacked((vendor.password))));
-    }
-
+    
     function checkAdminCredentials(string memory password)
         public
         view
